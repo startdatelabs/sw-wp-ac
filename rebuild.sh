16eed4cab695b82a5f71ada8d61b01d4dc521dd9
Siarhei Lazakovich <destryktorslk@gmail.com>
2019-05-20 10:40:18 +0300

    [SW] add dockerization & aws build cfg & cfg

---
+++ rebuild.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+docker build -t sw-wp-ac --no-cache .
+
+$(aws ecr get-login --no-include-email)
+
+tag=$(git rev-parse HEAD)
+docker tag sw-wp-ac:latest 075867688074.dkr.ecr.us-east-1.amazonaws.com/sw-wp-ac:$tag
+docker push 075867688074.dkr.ecr.us-east-1.amazonaws.com/sw-wp-ac:$tag
+
+docker tag sw-wp-ac:latest 075867688074.dkr.ecr.us-east-1.amazonaws.com/sw-wp-ac:latest
+docker push 075867688074.dkr.ecr.us-east-1.amazonaws.com/sw-wp-ac:latest
